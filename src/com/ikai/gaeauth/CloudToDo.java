9316a6b82a876d0d31fb40f42729be837c62dcdb
Ikai Lan <ikai.lan@gmail.com>
2010-08-23 15:36:33 -0700

    Initial commit

---
+++ src/com/ikai/gaeauth/CloudToDo.java
@@ -0,0 +1,13 @@
+package com.ikai.gaeauth;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CloudToDo extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
